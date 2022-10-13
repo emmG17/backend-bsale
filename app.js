bd9dda875046dba9ff91bbffc2b56b3e1ac855b5
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-10-12 21:12:51 -0500

    Add REST endpoints to get products and categories for the store

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import express from "express";
 
 // GET DB Access credentials
 dotenv.config();
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  poolLimit: 10,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
@@ -13,15 +14,71 @@ const connection = mysql.createConnection({
 
 const app = express();
 
-app.get("/allProducts", (req, res) => {
-  connection.connect();
-  connection.query("SELECT * FROM product", (err, rows, fields) => {
-    connection.end();
+// GET the list of all products
+app.get("/products/", (req, res) => {
+  pool.query("SELECT * FROM product", (err, rows, fields) => {
     if (err) throw err;
     res.json({ products: rows });
   });
 });
 
-app.listen(3000, () => {
+// GET all products with a similar name
+app.get("/products/:name", (req, res) => {
+  let name = req.params.name;
+  pool.query(
+    `SELECT * FROM product WHERE product.name LIKE ${pool.escape(name)}`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.json({ products: rows });
+    }
+  );
+});
+
+// GET a product by ID
+app.get("/product/:id", (req, res) => {
+  let id = req.params.id;
+  if (!id) res.json({});
+  pool.query(
+    `SELECT * FROM product WHERE product.id = ${pool.escape(id)}`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.json({ product: rows[0] });
+    }
+  );
+});
+
+// GET a product belonging to a category
+app.get("/products/category/:id", (req, res) => {
+  let categoryId = req.params.id;
+  pool.query(
+    `SELECT * FROM product WHERE product.category = ${pool.escape(categoryId)}`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.json({ products: rows });
+    }
+  );
+});
+
+// GET ALL categories
+app.get("/categories/", (req, res) => {
+  pool.query("SELECT * FROM category", (err, rows, fields) => {
+    if (err) throw err;
+    res.json({ categories: rows });
+  });
+});
+
+// GET a category by ID
+app.get("/category/:id", (req, res) => {
+  let id = req.params.id;
+  pool.query(
+    `SELECT * FROM category WHERE category.id = ${pool.escape(id)}`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.json({ category: rows[0] });
+    }
+  );
+});
+
+app.listen(3000 || process.env.PORT, () => {
   console.log("Listening on port 3000");
 });
